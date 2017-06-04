1284946e5aace1ca7f4ccd5ac71bcb8c0e9652dc
Oded_navon <oded.navon@gmail.com>
2017-06-04 22:35:59 +0300

    Look Great: Transparency, Room1 Look OK: Spheres, Pool Looks like shit: Triangle

--- src/RayTracing/RayTracer.java
+++ src/RayTracing/RayTracer.java
@@ -83,13 +83,13 @@ public class RayTracer {
 
 		for(int x=0; x<pixelPlane.getImageWidth(); x++){
 			for(int y=0; y<pixelPlane.getImageHeight(); y++){
-				System.out.println("x: "+x+" Y: "+y);
+				//System.out.println("x: "+x+" Y: "+y);
 				if(y==499){
-					System.out.println("got here");
+					//System.out.println("got here");
 				}
 				ray = pixelPlane.constructRayTroughPixel(x,y);
-				rgb = superSample(ray);
-//				rgb = scene.computeRGBForRay(ray,0).getRGB();
+				//rgb = superSample(ray);
+				rgb = scene.computeRGBForRay(ray,0).getRGB();
 				pixelPlane.setPixelColor(x,y,rgb);
 			}
 		}
